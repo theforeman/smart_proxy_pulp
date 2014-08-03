fe10bdbdea9e98c413ba4789c2ed4dea8f2a294d
Justin Sherrill <jsherril@redhat.com>
2014-08-03 19:50:58 -0400

    adding pulp_node feature
    Also changing default pulp URL to SSL

--- lib/smart_proxy_pulp.rb
+++ lib/smart_proxy_pulp.rb
@@ -1,2 +1,3 @@
 require 'smart_proxy_pulp_plugin/version'
 require 'smart_proxy_pulp_plugin/pulp_plugin'
+require 'smart_proxy_pulp_plugin/pulp_node_plugin'
--- lib/smart_proxy_pulp_plugin/pulp_plugin.rb
+++ lib/smart_proxy_pulp_plugin/pulp_plugin.rb
@@ -1,9 +1,9 @@
 module PulpProxy
   class Plugin < ::Proxy::Plugin
-    plugin :pulp, ::PulpProxy::VERSION
-    default_settings :pulp_url => 'http://localhost/pulp'
+    plugin "pulp", ::PulpProxy::VERSION
+    default_settings :pulp_url => 'https://localhost/pulp'
 
-    http_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
-    https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
+    http_rackup_path File.expand_path("pulp_http_config.ru", File.expand_path("../", __FILE__))
+    https_rackup_path File.expand_path("pulp_http_config.ru", File.expand_path("../", __FILE__))
   end
 end
