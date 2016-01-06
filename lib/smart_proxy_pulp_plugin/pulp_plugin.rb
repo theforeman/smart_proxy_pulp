7b4d074b904bedaa595ac53986f579bff14882b8
Shlomi Zadok <shlomi@ben-hanna.com>
2016-01-06 11:41:45 +0200

    Fixes #13013 - Adds API to display pulp dir status

--- lib/smart_proxy_pulp_plugin/pulp_plugin.rb
+++ lib/smart_proxy_pulp_plugin/pulp_plugin.rb
@@ -1,8 +1,12 @@
 module PulpProxy
   class Plugin < ::Proxy::Plugin
     plugin "pulp", ::PulpProxy::VERSION
-    default_settings :pulp_url => 'https://localhost/pulp'
+    default_settings :pulp_url => 'https://localhost/pulp',
+                     :pulp_dir => '/var/lib/pulp',
+                     :pulp_content_dir => '/var/lib/pulp/content',
+                     :mongodb_dir => '/var/lib/mongodb'
 
+    validate_readable :pulp_dir, :pulp_content_dir, :mongodb_dir
     http_rackup_path File.expand_path("pulp_http_config.ru", File.expand_path("../", __FILE__))
     https_rackup_path File.expand_path("pulp_http_config.ru", File.expand_path("../", __FILE__))
   end
