e0bb43c6fdb4043c25049d0c1b9741ed383bf96b
Stephen Benjamin <stephen@redhat.com>
2016-08-31 15:18:26 -0400

    refs #16008 - Add support for configurable puppet_content_dir

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ require "test/unit"
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'smart_proxy_for_testing'
+
+# create log directory in our (not smart-proxy) directory
+FileUtils.mkdir_p File.dirname(Proxy::SETTINGS.log_file)
