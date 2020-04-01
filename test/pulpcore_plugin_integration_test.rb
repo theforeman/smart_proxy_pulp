02f2cef13ab1e3b6327048af2264751d35390f51
Markus Bucher <bucher@atix.de>
2020-04-01 13:02:51 +0200

    Fixes #29449 - add credentials for pulpcore

--- test/pulpcore_plugin_integration_test.rb
+++ test/pulpcore_plugin_integration_test.rb
@@ -27,6 +27,37 @@ class PulpcoreFeaturesTest < Test::Unit::TestCase
     expected_settings = {
       'pulp_url' => 'http://pulpcore.example.com/foo',
       'content_app_url' => 'http://pulpcore.example.com:24816/',
+      'username' => nil,
+      'password' => nil,
+      'mirror' => false
+    }
+    assert_equal(expected_settings, pulpcore['settings'])
+    assert_equal(['foo'], pulpcore['capabilities'])
+  end
+
+  def test_features_with_optional_params
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('pulp.yml').returns(enabled: true, pulp_url: 'http://pulp.example.com/foo')
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('pulpnode.yml').returns(enabled: true, pulp_url: 'http://pulpnode.example.com/foo')
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('pulpcore.yml').returns(
+      enabled: true,
+      pulp_url: 'http://pulpcore.example.com/foo',
+      content_app_url: 'http://pulpcore.example.com:24816/',
+      username: 'admin',
+      password: 'password'
+    )
+    PulpProxy::PulpcoreClient.stubs(:capabilities).returns(['foo'])
+
+    get '/features'
+    response = JSON.parse(last_response.body)
+    pulpcore = response['pulpcore']
+
+    assert_equal 'running', pulpcore['state']
+
+    expected_settings = {
+      'pulp_url' => 'http://pulpcore.example.com/foo',
+      'content_app_url' => 'http://pulpcore.example.com:24816/',
+      'username' => 'admin',
+      'password' => 'password',
       'mirror' => false
     }
     assert_equal(expected_settings, pulpcore['settings'])
