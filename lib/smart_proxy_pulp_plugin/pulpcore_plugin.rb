71a20068e66bf8ba83dfab0f2c3e90358420e24f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-06-24 15:59:08 +0200

    Expose the container registry URL
    Today Katello constructs this URL, but we're in a far better position to
    determine this. The installer can also explicitly set this since it
    already knows it.
    
    It also expands the sample config to further document what the settings
    are and how to use them. While not used in production by us (the
    installer overwrites it), it is helpful to provide context.

--- lib/smart_proxy_pulp_plugin/pulpcore_plugin.rb
+++ lib/smart_proxy_pulp_plugin/pulpcore_plugin.rb
@@ -4,6 +4,7 @@ module PulpProxy
   class PulpcorePlugin < ::Proxy::Plugin
     plugin "pulpcore", ::PulpProxy::VERSION
     default_settings :pulp_url => 'https://localhost',
+                     :container_registry_url => false,
                      :content_app_url => 'https://localhost:24816/',
                      :mirror => false,
                      :client_authentication => ['password', 'client_certificate']
@@ -15,9 +16,16 @@ module PulpProxy
     validate :content_app_url, :url => true
     validate :client_authentication, :include => AUTH_TYPES
 
+    load_programmable_settings do |setting|
+      if settings[:container_registry_url] == true
+        settings[:container_registry_url] = URI.join(settings[:pulp_url], '/pulpcore_registry')
+      end
+    end
+
     expose_setting :pulp_url
     expose_setting :mirror
     expose_setting :content_app_url
+    expose_setting :container_registry_url
     expose_setting :username
     expose_setting :password
     expose_setting :client_authentication
