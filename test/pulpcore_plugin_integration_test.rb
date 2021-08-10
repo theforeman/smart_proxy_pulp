c37e6a540f55def6949e48ed488d1eb7c48dfa90
William Bradford Clark <wclark@redhat.com>
2021-08-10 13:59:52 -0400

    expose rhsm_url setting

--- test/pulpcore_plugin_integration_test.rb
+++ test/pulpcore_plugin_integration_test.rb
@@ -15,7 +15,12 @@ class PulpcoreFeaturesTest < Test::Unit::TestCase
   end
 
   def test_features
-    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('pulpcore.yml').returns(enabled: true, pulp_url: 'http://pulpcore.example.com/foo', content_app_url: 'http://pulpcore.example.com:24816/')
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('pulpcore.yml').returns(
+      enabled: true,
+      pulp_url: 'http://pulpcore.example.com/foo',
+      content_app_url: 'http://pulpcore.example.com:24816/',
+      rhsm_url: 'https://rhsm.example.com/rhsm'
+    )
     PulpProxy::PulpcoreClient.stubs(:capabilities).returns(['foo'])
 
     get '/features'
@@ -31,7 +36,8 @@ class PulpcoreFeaturesTest < Test::Unit::TestCase
       'username' => nil,
       'password' => nil,
       'mirror' => false,
-      'client_authentication' => ['password', 'client_certificate']
+      'client_authentication' => ['password', 'client_certificate'],
+      'rhsm_url' => 'https://rhsm.example.com/rhsm'
     }
     assert_equal(expected_settings, pulpcore['settings'])
     assert_equal(['foo'], pulpcore['capabilities'])
@@ -43,7 +49,8 @@ class PulpcoreFeaturesTest < Test::Unit::TestCase
       pulp_url: 'http://pulpcore.example.com/foo',
       content_app_url: 'http://pulpcore.example.com:24816/',
       username: 'admin',
-      password: 'password'
+      password: 'password',
+      rhsm_url: 'https://rhsm.example.com/rhsm',
     )
     PulpProxy::PulpcoreClient.stubs(:capabilities).returns(['foo'])
 
@@ -60,7 +67,8 @@ class PulpcoreFeaturesTest < Test::Unit::TestCase
       'username' => 'admin',
       'password' => 'password',
       'mirror' => false,
-      'client_authentication' => ['password', 'client_certificate']
+      'client_authentication' => ['password', 'client_certificate'],
+      'rhsm_url' => 'https://rhsm.example.com/rhsm',
     }
     assert_equal(expected_settings, pulpcore['settings'])
     assert_equal(['foo'], pulpcore['capabilities'])
@@ -106,5 +114,19 @@ class PulpcoreFeaturesTest < Test::Unit::TestCase
 
     assert_equal 'failed', pulpcore['state'], failure
     assert_equal "Disabling all modules in the group ['pulpcore'] due to a failure in one of them: Parameter 'client_authentication' is expected to be one or more of [\"password\", \"client_certificate\", \"client_certificate_admin_only\"]", failure
+  end
+
+  def test_invalid_rhsm_url
+    Proxy::DefaultModuleLoader.any_instance.expects(:load_configuration_file).with('pulpcore.yml').returns(enabled: true, rhsm_url: '')
+
+    get '/features'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    response = JSON.parse(last_response.body)
+    pulpcore = response['pulpcore']
+
+    failure = Proxy::LogBuffer::Buffer.instance.info[:failed_modules][:pulpcore]
+
+    assert_equal 'failed', pulpcore['state'], failure
+    assert_equal "Disabling all modules in the group ['pulpcore'] due to a failure in one of them: Parameter 'rhsm_url' is expected to have a non-empty value", failure
   end
 end
