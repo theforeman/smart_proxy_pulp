8dd60cc4cd320799687fe1cf7b3f83c96b25daed
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-09-13 12:55:03 +0200

    Fix Layout/SpaceBeforeBlockBraces and Layout/SpaceInsideBlockBraces cops

--- lib/smart_proxy_pulp_plugin/pulpcore_client.rb
+++ lib/smart_proxy_pulp_plugin/pulpcore_client.rb
@@ -14,7 +14,7 @@ module PulpProxy
 
     def self.capabilities
       body = JSON.parse(get("/pulp/api/v3/status/").body)
-      body['versions'].map{|item| item['component'] }
+      body['versions'].map { |item| item['component'] }
     rescue => e
       logger.error("Could not fetch capabilities: #{e.message}")
       []
