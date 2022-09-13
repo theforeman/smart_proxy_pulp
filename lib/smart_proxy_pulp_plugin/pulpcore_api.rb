96496dca25f9ef56cf811df00970c1e0e0c7c390
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-09-13 12:54:31 +0200

    Fix Layout/EmptyLineAfterGuardClause cop

--- lib/smart_proxy_pulp_plugin/pulpcore_api.rb
+++ lib/smart_proxy_pulp_plugin/pulpcore_api.rb
@@ -10,6 +10,7 @@ module PulpProxy
       begin
         result = PulpcoreClient.get("/pulp/api/v3/status/")
         return result.body if result.is_a?(Net::HTTPSuccess)
+
         log_halt result.code, "Pulp server at #{PulpcoreClient.pulp_url} returned an error: '#{result.message}'"
       rescue SocketError, Errno::ECONNREFUSED => e
         log_halt 503, "Communication error with '#{URI.parse(PulpcoreClient.pulp_url).host}': #{e.message}"
