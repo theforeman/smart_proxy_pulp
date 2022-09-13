1a8264f2e543f59f198259b1737e71aa4349d888
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-09-13 13:10:57 +0200

    Fix Style/GuardClause cop

--- lib/smart_proxy_pulp_plugin/validators.rb
+++ lib/smart_proxy_pulp_plugin/validators.rb
@@ -3,12 +3,9 @@ module PulpProxy
     class Include < ::Proxy::PluginValidators::Base
       def validate!(settings)
         setting_value = settings[@setting_name]
+        return true if (setting_value - @params).empty?
 
-        unless (setting_value - @params).empty?
-          raise ::Proxy::Error::ConfigurationError, "Parameter '#{@setting_name}' is expected to be one or more of #{@params}"
-        else
-          true
-        end
+        raise ::Proxy::Error::ConfigurationError, "Parameter '#{@setting_name}' is expected to be one or more of #{@params}"
       end
     end
   end
